03bbb7e29d40caea308b7e4792118205a6b31f7f
Sekongo <sekongoismael0@gmail.com>
2023-11-28 14:17:35 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,5 @@
 def print_reverse_alphabet():
     for i in range(ord('z'), ord('a') - 1, -1):
             print("{}".format(chr(i).lower())
-                    if i % 2 == 0 else "{}".format(chr(i).upper()), end="")
+            if i % 2 == 0 else "{}".format(chr(i).upper()), end="")
 print_reverse_alphabet()            
