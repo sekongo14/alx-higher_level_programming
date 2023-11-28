ac0bf6eeecd432698d5f027a0cf80190b636a8bb
Sekongo <sekongoismael0@gmail.com>
2023-11-28 07:23:30 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for y in range(i + 1, 10):
         if i == 8 and y == 9:
             print("{:d}{:d}".format(i, y), end="")
-        else:    
+        else:
             print("{:d}{:d}, ".format(i, y), end="")
-print()        
+print()
