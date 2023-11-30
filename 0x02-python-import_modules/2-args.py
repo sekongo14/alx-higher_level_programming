f0c5289baff1921d830b041bb0aaa984c7592f7b
Sekongo <sekongoismael0@gmail.com>
2023-11-30 02:56:24 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+lene = len(sys.argv) - 1
+if lene == 1:
+    print("{:d} argument:".format(lene))
+elif lene == 0:
+    print("{:d} arguments.".format(lene))
+else:
+    print("{:d} arguments:".format(lene))
+for i in range(1, lene+1):
+    print("{:d}: {}".format(i, sys.argv[i]))
