a8c317c1e2b051134f266561c3a5fff8a56f61da
Sekongo <sekongoismael0@gmail.com>
2023-11-30 06:03:06 -0800

    103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    print(''.join(chr(i) for i in range(ord('A'), ord('Z')+1)), end='\n')
+    print(*map(chr, range(ord('A'), ord('Z')+1)), sep="", end='\n')
