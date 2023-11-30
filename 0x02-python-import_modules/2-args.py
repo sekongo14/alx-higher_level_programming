27f77c535ff06e2c4afaad2ccac04be972a52f78
Sekongo <sekongoismael0@gmail.com>
2023-11-30 04:51:10 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import sys
-lene = len(sys.argv) - 1
-if lene == 1:
-    print("{:d} argument:".format(lene))
-elif lene == 0:
-    print("{:d} arguments.".format(lene))
-else:
-    print("{:d} arguments:".format(lene))
-for i in range(1, lene+1):
-    print("{:d}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+
+    import sys
+    lene = len(sys.argv) - 1
+    if lene == 1:
+        print("{:d} argument:".format(lene))
+    elif lene == 0:
+        print("{:d} arguments.".format(lene))
+    else:
+        print("{:d} arguments:".format(lene))
+    for i in range(1, lene+1):
+        print("{:d}: {}".format(i, sys.argv[i]))
