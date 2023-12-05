f92802b01424ad5fe68a756e03a9e25140bcc1c6
Sekongo <sekongoismael0@gmail.com>
2023-12-05 03:54:25 -0800

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for i in range(len(my_list)):
+        new_list.append(my_list[i])
+        if (my_list[i] % 2 == 0):
+            return True
+        else:
+            return False 
