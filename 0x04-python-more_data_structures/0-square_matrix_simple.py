f9e73ead8d669bf9f632aaf5fb56c43c11b05936
Sekongo <sekongoismael0@gmail.com>
2023-12-07 00:18:41 -0800

    0-S

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_mat = []
+    for i in range(len(matrix)):
+        row = []
+        for j in range(len(matrix[i])):
+            row.append((matrix[i][j])**2)
+        new_mat.append(row)    
+    return new_mat        
