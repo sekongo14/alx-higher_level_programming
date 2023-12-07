ba9458209d0de6ef78b4e49ef1dcdb04e90521ec
Sekongo <sekongoismael0@gmail.com>
2023-12-07 04:42:42 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    keys = max(a_dictionary.keys())
-    return keys
+    if not a_dictionary:
+        return None
+
+    max_key = None
+    max_value = float('-inf')
+
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+
+    return max_key
