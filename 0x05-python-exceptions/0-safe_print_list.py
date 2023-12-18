e6693a4810e7557cf93636f10cfd2d1803e28f2b
Sekongo <sekongoismael0@gmail.com>
2023-12-18 02:47:13 -0800

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+   longe = 0
+   for y in range(x):
+       try:
+           print(my_list[y], end="")
+           longe += 1
+       except IndexError:
+           pass
+   return longe     
