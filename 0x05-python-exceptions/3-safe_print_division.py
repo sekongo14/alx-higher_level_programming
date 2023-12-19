d617e599133d1fa6c597f08156e34178830da4bf
Sekongo <sekongoismael0@gmail.com>
2023-12-19 03:01:10 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except(TypeError ,ZeroDivisionError):
-        return None
+        result = None
     finally:
-        if 'result' not in locals():
-            result = None
         print("Inside result {}".format(result))
     return result
