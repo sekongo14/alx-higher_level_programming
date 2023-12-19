c8ca5fac087d1f5d52f02a68ceef531b6a84dd85
Sekongo <sekongoismael0@gmail.com>
2023-12-19 02:34:18 -0800

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    div = 0
+    try:
+        div = a / b
+        return div
+    except(ValueError, TypeError):
+        return None
+    finally:
+        print("Inside result {}".format(div))
+        print("{} / {} = {}".format(a, b, div))
