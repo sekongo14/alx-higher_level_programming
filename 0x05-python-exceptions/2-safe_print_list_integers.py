3ce730795660f9ecff2606b6205ab0f28818988b
Sekongo <sekongoismael0@gmail.com>
2023-12-19 02:05:32 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     longe = 0
     for y in range(x):
         try:
-            print(my_list[y], end="")
+            print("{:d}".format(my_list[y]), end="")
             longe += 1
         except (IndexError, TypeError, ValueError):
             pass
