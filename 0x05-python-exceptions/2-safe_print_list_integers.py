4848845113bd41ce3dabeaf9ed96cac1917ce7f6
Sekongo <sekongoismael0@gmail.com>
2023-12-19 01:59:57 -0800

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    longe = 0
+    for y in range(x):
+        try:
+            print(my_list[y], end="")
+            longe += 1
+        except (IndexError, TypeError, ValueError):
+            pass
+    print()
+    return longe
